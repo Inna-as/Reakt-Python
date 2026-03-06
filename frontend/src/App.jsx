dc136dcccafae87f2b946ceb1b06462b991baa08
inna <aleksandr9311737@mail.ru>
2026-03-06 21:04:11 +0300

    Добавила Руфвук и подключила в AddHeader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react"
+import { useState } from 'react'
+
+import Header from './components/UI/Header/Header.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 return (
 <>
+  <Header />
   <h1>Счетчик кликов</h1>
     <button onClick={() => setCount(count + 1)}>
       Клик: {count}
---
+++ frontend/src/components/UI/Header/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    return <header>Шапка сайта</header>   
+}
+
+export default Header
