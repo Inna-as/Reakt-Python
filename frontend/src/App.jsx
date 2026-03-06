254f511d28bf0aede77ef6890e883c05bccf8019
inna <aleksandr9311737@mail.ru>
2026-03-06 18:27:40 +0300

    E: добавила счетчик кликов

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <div>Hello World!</div>
-}
+import { useState } from "react"
 
+function App() {
+const[count, setCount] = useState(0)
+return<div>
+  <h1>Счетчик кликов</h1>
+    <button onClick={() => setCount(count + 1)}>
+      Клик: {count}
+    </button>
+</div>
+}
 export default App
