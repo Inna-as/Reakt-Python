faa53d6dedb01f7be08f1ec23908d5ea934abf0f
inna <aleksandr9311737@mail.ru>
2026-03-10 18:24:02 +0300

    событие onMausEnter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,14 @@ import Header from '@components/UI/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
+  const pHover = () => { 
+  console.log("Нажали на р");
+    
+  }
 return (
 <>
   <Header />
+  <p onMouseEnter={pHover}>Hello World!</p>
   <h1>Счетчик кликов</h1>
     <button onClick={() => setCount(count + 1)}>
       Клик: {count}
